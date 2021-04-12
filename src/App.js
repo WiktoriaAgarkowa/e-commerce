564a01c4412bf8d9c027e3647496daba38787e46
wagarkowa <victoriaagarkova96@gmail.com>
2021-04-12 21:05:30 +0200

    created form-input and custom-button components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignOut from './pages/sign-in-sign-up/sign-in-sign-up.component';
 import Header from './components/header/header.component';
+
 
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInAndSignOut} />
       </Switch>
     </div>
   );
